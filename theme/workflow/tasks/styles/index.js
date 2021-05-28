cc0ae03ccd89fa625bae2ce51bbd2e96c100f489
salexzee <510samwebb@gmail.com>
2021-05-28 17:13:53 -0500

    utilizes autoprefixer in styles task

--- theme/workflow/tasks/styles/index.js
+++ theme/workflow/tasks/styles/index.js
@@ -2,12 +2,18 @@ const { src, dest } = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
 
-sass.compiler = require('node-sass')
+const prefixerOptions = {
+  cascade: false,
+};
+
+sass.compiler = require('node-sass');
 
 const styles = () => src('src/styles/theme.scss')
   .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
+  .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+  .pipe(autoprefixer(prefixerOptions))
   .pipe(sourcemaps.write())
   .pipe(rename('theme.css'))
   .pipe(dest('dist/assets/'));
